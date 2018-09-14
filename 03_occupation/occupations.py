2fbf90092f13ceb686ee43910f1d742af69e3720
wzhou2 <33831042+wzhou2@users.noreply.github.com>
2018-09-14 13:28:51 -0400

    match thomas' updates

--- 03_occupation/occupations.py
+++ 03_occupation/occupations.py
@@ -1,37 +1,66 @@
-# Wei Wen Zhou and Thomas Zhao
+# Wei Wen Zhou, Thomas Zhao
 # SoftDev1 pd8
 # K#06: StI/O: Divine your Destiny!
 # 2018-09-13
+# Team Double Z Dynasty
 
-file = open("occupations.csv",'r')
+import random
 
-info = file.read().split("\n")
+def pickOccupation():
+    file = open("occupations.csv",'r')
+    info = file.read().split("\n")
+    file.close()
+    
+    info.pop(0) # Title line
+    info.pop(len(info)-1) # Last line
+    info.pop(len(info)-1) # again due to some issue with csv
+    
+    print('info length:')
+    print(len(info))
+    
+    # Initialize dictionary
+    book = {}
+    for index in range(0,len(info)):
+        if info[index].count(',') == 1:
+            line = info[index].split(",")
+            book[float(line[1])] = (line[0])
+        else: 
+            # more than 1 comma
+            lastC = info[index].rindex(',')
+            book[float(info[index][lastC+1:len(info[index])])] = info[index][0:lastC]
+            #print(info[index])
+            #print(float(info[index][lastC+1:len(info[index])]))
 
-file.close()
-# Title line
-info.pop(0)
-# Last line
-info.pop(len(info)-1)
+    print(book)
+    print('book len')        
+    print(len(book))
 
-# Initialize dictionary
-book = {}
-for x in range(0,len(info)):
-    if info[x].count(',') == 1:
-        line = info[x].split(",")
-        book[line[0]] = float(line[1])
-    else: 
-    # more than 1 comma
-        lastC = info[x].rindex(',')
-        book[info[x][0:lastC]] = float(info[x][lastC+1:len(info[x])-1])
-#print (book)
+    #debug
+    all = 0
+    for key in book:
+        all = all + float(key)
+    print('total: ')
+    print(all)
+    #debug
+    
+    target = random.uniform(0, 99.8)
+    print(target)
+    total = 0
 
-# Added Thomas' part
-names = []
-weights = []
+    print('length of book:')
+    print(len(book))
+    
+    for key in book:
+        total = total + float(key)
+        print(total)
+        if total > 85.9 + 6.1 and total < 92:
+            return 'Education'
+        if total > target:
+            print(book[key])
+            return book[key]
         
-for key, value in book.iteritems():
-    i = 0
-    names.append(key)
-    weights.append(value)
 
-print(random.choices(names, weights, k=1))
+        
+    #return(random.choices(names, weights, k=1))
+
+print(pickOccupation())
