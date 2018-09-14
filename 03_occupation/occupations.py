714c599bebb80ea584edfbf8a831de4e8753efc3
wzhou2 <wzhou2@stuy.edu>
2018-09-13 23:59:27 -0400

    wrote the dictionary creation script and tested

--- 03_occupation/occupations.py
+++ 03_occupation/occupations.py
@@ -3,20 +3,25 @@
 # K#06: StI/O: Divine your Destiny!
 # 2018-09-13
 
-file = open("occupation.csv",r)
+file = open("occupations.csv",'r')
 
 info = file.read().split("\n")
 
 file.close()
 # Title line
 info.pop(0)
-# Last line
-info.pop(len(info-1))
+# Last line : blank plus total
+info.pop(len(info)-1)
+info.pop(len(info)-1)
 
 
 book = {}
 for x in range(0,len(info)):
-    line = info[x].split(",")
-    book[line[0]] = line[1]
-
-print book;
+    if info[x].count(',') == 1:
+        line = info[x].split(",")
+        book[line[0]] = float(line[1])
+    else: 
+    # more than 1 comma
+        lastC = info[x].rindex(',')
+        book[info[x][0:lastC]] = float(info[x][lastC+1:len(info[x])-1])
+#print (book)
