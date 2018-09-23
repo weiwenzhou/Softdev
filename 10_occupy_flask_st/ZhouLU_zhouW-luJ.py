72d7d007c74ff1c79b263a3ff73ac722f4c890c6
wzhou2 <wzhou2@stuy.edu>
2018-09-23 18:00:57 -0400

    Added ZhouDyansty occupation.py code :D

--- 10_occupy_flask_st/ZhouLU_zhouW-luJ.py
+++ 10_occupy_flask_st/ZhouLU_zhouW-luJ.py
@@ -1,6 +1,44 @@
+# ZhouLu - Wei Wen Zhou, Jack Lu
+#SoftDev1 pd8
+#K# 10: Jinja Tuning 
+#2018-09-23 
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+# From ZhouDynasty work - #3
+def pickOccupation():
+    file = open("occupations.csv",'r')
+    info = file.read().split("\n")
+    file.close()
+    
+    info.pop(0) # Title line
+    info.pop(len(info)-1) # Last line
+    info.pop(len(info)-1) # again due to some issue with csv
+    
+    #print('info length:')
+    #print(len(info))
+    
+    # Initialize dictionary
+    book = {}
+    for x in range(0,len(info)):
+        if info[x].count(',') == 1:
+            line = info[x].split(",")
+            book[line[0]] = float(line[1])
+        else: 
+            # more than 1 comma
+            lastC = info[x].rindex(',')
+            book[info[x][0:lastC]] = float(info[x][lastC+1:len(info[x])])
+
+    target = random.uniform(0, 99.8)
+    current = 0
+
+    for entry in book:
+        current = current + book[entry]
+        if current > target:
+            return entry
+
 
 @app.route('/')
 def index():
