47ed8a80f486ddb37cc6f72d6b6a28f274839c1b
wzhou2 <wzhou2@stuy.edu>
2018-10-08 23:30:07 -0400

    match partner's copy

--- 17_db-nmcrnch/stu_mean.py
+++ 17_db-nmcrnch/stu_mean.py
@@ -1,86 +1,93 @@
-#Jiggumbob - Wei Wen Zhou, Soojin Choi 
-#SoftDev1 pd8
-#K #17: Average
-#2018-10-05
+# Jiggumbob - Wei Wen Zhou, Soojin Choi
+# SoftDev1 pd08
+# K17 : Average
+# 2018-10-05
 
-# Jiggumbob - a thingamabob; a gadget; a whatsit; a gewgaw 
+import sqlite3
+DB_FILE = "discobandit.db"
 
-import sqlite3   #enable control of an sqlite database
-import csv       #facilitates CSV I/O
+db = sqlite3.connect(DB_FILE)
+c = db.cursor()
 
+#dictionary that holds all the students' data
+studentGrades = {}
 
-DB_FILE="discobandit.db"
+#looks up the students grade and keeps them.
+#studentsGrades should be studentName : [id , [grades...]]
+def look_up():
+    name_id = c.execute("SELECT name, id FROM peeps;")
 
-db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
-c = db.cursor()               #facilitate db ops
+    #loops through to get the keys and id for each student
+    for each_peeps in name_id:
 
-#==========================================================
-#INSERT YOUR POPULATE CODE IN THIS ZONE
+        peepsName = each_peeps[0]
+        peepsId = each_peeps[1]
+        studentGrades[peepsName] = peepsId
 
-#Look up each student's grades
+    #loops through the dictionary and pulls the id data 
+    for each in studentGrades:
+        targetID = studentGrades[each]
 
-def gradeLookup(id):
-    command = "SELECT courses.name, mark FROM peeps, courses WHERE peeps.id = {0} AND courses.id = {0}".format(id)
-    c.execute(command)
-    grades = "Grades of {0}\n".format(id)
-    for each in c:
-        grades += "{0}, {1}\n".format(each[0], each[1])
-    return grades
+        info = [targetID]
+        grade = []
+        id_grades = c.execute("SELECT id, mark FROM courses")  
+
+        #Then loops through the courses table to pull grades that match the student's id.
+        for each_courses in id_grades:
+        
+            courseId = each_courses[0]
+            courseGrade = each_courses[1]
+        
+            if targetID == courseId:
+                grade.append(courseGrade)
+            
+        info.append(grade)
+        studentGrades[each] = info
+
+look_up()
+
+#Removes the grades of the student and replaces it with the average of the grades.
+# studentGrades should be studentName : [id , avgGrade]
+def avg_grade():
+    for each in studentGrades:
     
-print(gradeLookup(2))
+        average = 0
+        counter = 0
+        for grade in studentGrades[each].pop():
+            average += grade
+            counter += 1
 
-#Compute each student's average
-def getAverage(id):
-    command = "SELECT mark FROM peeps, courses WHERE peeps.id = {0} AND courses.id = {0}".format(id)
-    c.execute(command)
-    total = 0.0
-    count = 0
-    for each in c:
-        total += each[0]
-        count += 1
-    return total/count
+            #calculates the avg
+            average = average / counter
+            studentGrades[each].append(average)
 
-print(getAverage(2))
-print()
-    
-#Display each student's name, id, and average
+avg_grade()
+
+#displays the student's name, id and average grade
 def display():
-    command = "SELECT * FROM peeps"
-    c.execute(command)
-    dict = {}
-    for each in c: 
-        dict[each[0]] = each[2]
-    table = "Name, id, average\n"    
-    for each in dict:
-        table += "{0}, {1}, {2}\n".format(each, dict[each], getAverage(dict[each]))
-    return table
+    command = ""
+    for student in studentGrades:
+        avgGrade = studentGrades[student][1]
+        id = studentGrades[student][0]
+        command = "Student name: {0} | ID: {1} | Grade: {2}". format(student, id , avgGrade)
+        print (command)
 
-print(display())
+display()
+
+#creates a table ,peeps_avg, that inserts the values of id and avgGrade
+def create_table():
+    command = "CREATE TABLE peeps_avg (id INTEGER PRIMARY KEY, grade INTEGER)"
+    c.execute(command)
+
+    for student in studentGrades:
+        avgGrade = studentGrades[student][1]
+        id = studentGrades[student][0]
+        command = "INSERT INTO peeps_avg values ({0}, {1})".format(id, avgGrade)
+        c.execute(command)
+
+create_table()
+
+#adds to the courses table, a course name, student id and mark as one row.
+def add_to_courses(courseName, studentId, mark):
+    c.execute("INSERT INTO courses vales (\"{0}\", {1}, {2})".format(courseName, mark, studentId) )
     
-#Create a table of IDs and associated averages, named "peeps_avg"
-def createTable():
-    command = "CREATE TABLE peeps_avg (id INTEGER PRIMARY KEY, avg FLOAT);"
-    c.execute(command)
-    command = "SELECT * FROM peeps"
-    c.execute(command)
-    dict = {}
-    for each in c: 
-        dict[each[0]] = each[2]
-    for each in dict:
-       c.execute('INSERT INTO peeps_avg values({0}, {1})'.format(dict[each], getAverage(dict[each])))
-
-createTable()
-       
-#Facilitate adding rows to the courses table
-def addCourse(name, mark, id):
-    command = 'INSERT INTO courses values ("{0}", {1}, {2})'.format(name, mark, id)
-    c.execute(command)
-    
-addCourse("apcs", 95, 2)
-print(gradeLookup(2))
-print(getAverage(2))
-
-#==========================================================
-
-db.commit() #save changes
-db.close()  #close database
