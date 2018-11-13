30c065e11edcd6f98565cf51ae3a080d5396ae92
wzhou2 <wzhou2@stuy.edu>
2018-11-13 14:05:42 -0500

    base

---
+++ 24_rest/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from urllib import request
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return render_template("index.html", )
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
