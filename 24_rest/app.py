cba4077ac763474e0ad50f82f3d9cf18a37ef081
wzhou2 <wzhou2@stuy.edu>
2018-11-13 14:08:50 -0500

    base

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 from urllib import request
+import json
 
 app = Flask(__name__)
+
+fd = urllib.request.urlopen("https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2014-02-01&cloud_score=True&api_key=DEMO_KEY")
 
 @app.route("/")
 def root():
