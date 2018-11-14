a26595fb0c9ea1e3ff98a50c6e1a0ce7809ed17b
wzhou2 <wzhou2@stuy.edu>
2018-11-13 23:23:52 -0500

    finish

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template
-from urllib import request
+import urllib.request, urllib.parse
 import json
 
 app = Flask(__name__)
 
-fd = urllib.request.urlopen("https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2014-02-01&cloud_score=True&api_key=DEMO_KEY")
-
 @app.route("/")
 def root():
-    return render_template("index.html", )
+    fd = urllib.request.urlopen("https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2014-02-01&cloud_score=True&api_key=DEMO_KEY")
+    data = fd.read()
+    dict = json.loads(data)
+    
+    return render_template("index.html", url=dict['url'])
 
 if __name__ == "__main__":
     app.debug = True
