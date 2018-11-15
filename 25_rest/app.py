9501d0a8991a5f6a6baf7d094078950c3198770d
wzhou2 <wzhou2@stuy.edu>
2018-11-14 23:41:58 -0500

    finish

--- 25_rest/app.py
+++ 25_rest/app.py
@@ -14,15 +14,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    #KEY_NASA = "DEMO_KEY"
-    KEY_NASA = "L1NmAHRWyqjuPbhLPAKh6b7R2MEaTqtiMKCLaLd0"
-    URL_STUB = "https://api.nasa.gov/planetary/apod?api_key="
-    URL = URL_STUB + KEY_NASA
-    fd = request.urlopen(URL)
-    data = fd.read()
-    dict = json.loads(data)
-    
-    return render_template("index.html", url=dict['url'])
+    KEY = "ba27b5fd166c4ca6a538cbb26de45975"
+    URL_STUB = "https://services.last10k.com/v1/company/MSFT/quote?Subscription-Key="
+    URL = URL_STUB + KEY
+    print("---------------")
+    print("URL:")
+    print(URL)
+    print("---------------")
+    u = request.urlopen(URL)
+    response = u.read()
+    print("---------------")
+    print("response:")
+    print(response)
+    print("---------------")
+    data = json.loads(response)
+
+    return render_template("index.html", name=data['Name'], symbol=data["Symbol"], s_exchange=data["StockExchange"], high=data["DailyHigh"], low=data["DailyLow"], change=data["Change"])
 
 if __name__ == "__main__":
     app.debug = True
