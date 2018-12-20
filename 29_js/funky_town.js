2b5d79c83bb8c9343e535fdda1cd8a61d3ec495f
wzhou2 <wzhou2@stuy.edu>
2018-12-19 22:10:45 -0500

    if we show up, we gon' show out

--- 29_js/funky_town.js
+++ 29_js/funky_town.js
@@ -5,11 +5,12 @@ K29 -- Sequential Progression
 */
 var fibonacci = function(n) {
     // Returns the nth Fibonacci number
-    if (n == 0) {
-        return 0;
-    }
-    if (n < 3) {
-        return 1;
+    
+    // Why think when you can hard-code it haha!
+    var list = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765];
+    
+    if (n < list.length) {
+        return list[n];
     } else {
         return fibonacci(n-1) + fibonacci(n-2);
     }
@@ -38,8 +39,8 @@ var randomStudent = function() {
 }
 
 var eventOne = function(){
-    // Generates a random integer, n, between [0,10) and changes the text to the nth Fibonacci number
-    randVal = Math.floor(Math.random() * 10);
+    // Generates a random integer, n, between [0,25) and changes the text to the nth Fibonacci number
+    randVal = Math.floor(Math.random() * 25);
     retVal = randVal + " -> " + fibonacci(randVal)+"<br>";
     console.log(retVal);
     var textbox = document.getElementById('log');
@@ -48,10 +49,34 @@ var eventOne = function(){
 };
 
 var wipes = function() {
+    // Resets the output section
     var textbox = document.getElementById('log');
     textbox.innerHTML = "Output: <br>"
+    console.log("CLEAR!");
 }
 
+var chooseYourFib = function() {
+    var textbox = document.getElementById('log');
+    // Allows only numbers
+    var num = parseInt(document.getElementById('number').value);
+    if (!Number.isInteger(num)) {
+        textbox.innerHTML = textbox.innerHTML + "Enter a number <br>";
+        console.log("Enter a number");
+        return;
+    }
+    // Limit the number of recursion calls.
+    if (num > 50) {
+        textbox.innerHTML = textbox.innerHTML + 'Stop yourself. This is not for your abuse <br> <a href="https://www.dcode.fr/fibonacci-numbers"> Find your nth Fibonacci number here </a> <br>';
+        console.log("Stop yourself!");
+        return;
+    }
+    
+    // console.log(num + "?");
+    retVal = num + " -> " + fibonacci(num)+"<br>";
+    console.log(retVal);
+    textbox.innerHTML = textbox.innerHTML + retVal;
+    return retVal;
+}
 // Please Click button
 var b = document.getElementById('button');
 // console.log(b);
@@ -59,3 +84,6 @@ b.addEventListener('click', eventOne);
 
 // Clear Output button
 document.getElementById('clear').addEventListener('click', wipes);
+
+// Choose your own nth Fibonacci number
+document.getElementById("numberB").addEventListener('click', chooseYourFib);
