3c7d6154080068f7eb9d750f8505ddb2969fbbed
wzhou2 <wzhou2@stuy.edu>
2018-12-19 21:30:42 -0500

    stop, wait a minute [I might be on to something - KEYS break]

--- 29_js/funky_town.js
+++ 29_js/funky_town.js
@@ -1,9 +1,10 @@
-/*Team _ - Wei Wen Zhou, Soojin Choi
+/*Team ArmorBuns - Wei Wen Zhou, Soojin Choi
 SoftDev1 pd8
 K29 -- Sequential Progression
 2018-12-19
 */
 var fibonacci = function(n) {
+    // Returns the nth Fibonacci number
     if (n == 0) {
         return 0;
     }
@@ -15,6 +16,7 @@ var fibonacci = function(n) {
 }
 
 var gcd = function(a, b) {
+    // Returns gcd of a, b
     var x;
     if (a < b) {
         x = a;
@@ -30,13 +32,25 @@ var gcd = function(a, b) {
 }
 
 var randomStudent = function() {
+    // Returns a random value from a list
     var list = ['Derek', 'Britni', 'Joan', 'Vincent', 'Jared', 'Ivan', 'Thomas', 'Maggie', 'Damian', 'Tina', 'Fabiha', 'John', 'Susan ', 'Kaitlin', 'Michelle', 'Clara', 'Rachel', 'Amit', 'Jerry', 'Raymond', 'Zane', 'Soojin', 'Maryann', 'Adil', 'Josh', 'Imad' ];
     return list[Math.floor(Math.random()*list.length)];
 }
 
-var fxn = function() {
-    b.nodeValue = "done";
-    return 1;
-}
-var b = document.getElementById("button");
-b.addEventListener("click", fxn);
+var eventOne = function(){
+    // Generates a random integer, n, between [0,10) and changes the text to the nth Fibonacci number
+    randVal = Math.floor(Math.random() * 10);
+    retVal = fibonacci(randVal)+"<br>";
+    console.log(retVal);
+    var textbox = document.getElementById('log');
+    textbox.innerHTML = textbox.innerHTML + retVal;
+    return retVal;
+};
+
+// Please Click button
+var b = document.getElementById('button');
+console.log(b);
+b.addEventListener('click', eventOne);
+
+// Clear Output button
+b.addEventListener('click', eventOne);
